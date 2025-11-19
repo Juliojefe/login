48b0a99471d37c95209fc9028d5413481bfd6930
Juliojefe <j.c.jf285@gmail.com>
2025-11-18 16:12:19 -0800

    set up

---
+++ index.mjs
@@ -0,0 +1,34 @@
+import express from 'express';
+import mysql from 'mysql2/promise';
+const app = express();
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+//for Express to get values using the POST method
+app.use(express.urlencoded({ extended: true }));
+//setting up database connection pool
+const pool = mysql.createPool({
+    host: "w1h4cr5sb73o944p.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+    user: "bp1t2waqynoo300o",
+    password: "xhvh50l86emzmr72",
+    database: "zutb6skwh6b157ly",
+    connectionLimit: 10,
+    waitForConnections: true
+});
+//routes
+app.get('/', (req, res) => {
+    console.log("Server Running");
+    res.render('login.ejs')
+});
+
+app.get("/dbTest", async (req, res) => {
+    try {
+        const [rows] = await pool.query("SELECT CURDATE()");
+        res.send(rows);
+    } catch (err) {
+        console.error("Database error:", err);
+        res.status(500).send("Database error!");
+    }
+});//dbTest
+app.listen(3000, () => {
+    console.log("Express server running")
+})
